64a4c6b726c7ec6f099c78097310b7fe62077d2f
adityamoneyg <158058588+adityamoneyg@users.noreply.github.com>
2024-07-16 10:47:30 +0530

    Add files via upload

---
+++ C++_Assignment_L1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Physics\n";
+    cout<<"Wallah";
+}
+
+//   2 solution
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<5+5;
+}
+
+//   3 solution
+
+int takes Typically 4 bytes (32 bits) on most systems
+bool takes  Typically 1 byte (8 bits)
+float takes Typically 4 bytes (32 bits)
+
+//   4 solution
+
+It will gives an error
+
+//   5 solution
+
+#include<iostream>
+using namespace std;
+int main() {
+int r;
+cout<<"radius "<<endl;
+cin>>r;
+cout<<"Perimeter of Circle is : "<< 2*3.14*r;
+return 0;
+}
+
+//    6 solution
+
+_FLOAT , FLOAT can be a valid variable name
